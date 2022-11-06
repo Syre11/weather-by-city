85ed9162185bbee689328d7a37c5f23e3e9228fc
“Spencer <srye1140@gmail.com>
2022-11-06 15:45:30 -0800

    Adds code to meet project requirements for functionality and adds styling

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# weather-by-city
+# Weather by City
+## Description
+
+This project was created to gain a deeper understanding of working with API's and furthering my skills in CSS, HTML, and Javascript. The webpage is a weather dashboard which will show current weather and a 5 day forecast for whatever city you search for. It also locally stores your previous searches.
+
+## Installation
+
+N/A
+
+## Usage
+
+Enter a city name into the search box and click search and you will be provided current weather and a 5 day forecast for that city.
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![Screenshot of the webpage](./assets/images/Screenshot%202022-11-06%20154017.png)
+
+## Credits
+
+Used the Open Weather Map API: https://openweathermap.org/
+
+Used Bootstrap v4.6: https://getbootstrap.com/docs/4.6/getting-started/introduction/
+
+Used Moment JS: https://momentjs.com/
+
+## License
+
+Please refer to the LICENSE file.
+
+## Features
+
+This project utilizes a third-party API to gather data and display it for the user. It also utilizes local storage to be able to see previous searches.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var cityInput = document.querySelector("#cityInput");
 var searchBtnEl = document.querySelector(".searchBtn");
 var todayResultsEl = document.querySelector(".todayResults")
+var prevSearchEl = document.querySelector(".prevSearch")
+var prevSearchBtn = document.querySelector(".btn-secondary")
 var fiveDayEl = document.querySelector(".fiveDay")
 var firstDayEl = document.querySelector(".firstDay")
 var secondDayEl = document.querySelector(".secondDay")
@@ -12,15 +14,23 @@ var resultsEl = document.querySelector(".results")
 var lat = "";
 var lon = "";
 
-
+//hides these on load
 resultsEl.setAttribute("style", "display: none")
+prevSearchEl.setAttribute("style", "display: none")
 
-
+//function to get lat and lon and start the getforecastApi and getTodayApi functions
 function getLocation() {
+  //capitalizes the first letter of each word in the city name
+  var cityArray = cityInput.value.split(" ");
+  for (var i = 0; i < cityArray.length; i++) {
+    cityArray[i] = cityArray[i].charAt(0).toUpperCase() + cityArray[i].slice(1);
+  }
+  var capInput = cityArray.join(" ");
+  cityInput.value = capInput
+
   //replaces spaces in user input with + to work with api url
   for (var i = 0; i < cityInput.value.length; i++) {
     cityInput.value = cityInput.value.replace(" ", "+")
-    console.log (cityInput.value)
   };
 
   var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&limit=1&appid=e4f7fa74cca6af69d01f1e01654e6cc3'
@@ -31,16 +41,83 @@ function getLocation() {
       })
       .then(function (jsonArray) {
         console.log(jsonArray);
+        //sets lat and lon, functions need to be called within this function in order to use the variables from this function
         lat = jsonArray[0].lat;
         lon = jsonArray[0].lon;
-        getforecastApi ()
-      })
 
+        //stores just the city name, will reuse this whole function to re-search for data, rather than storing all of the data
+        localStorage.setItem('prevCity', JSON.stringify(cityInput.value));
+
+        var city = localStorage.getItem('prevCity')
+        var storageArray = JSON.parse(localStorage.getItem('prevCity'))
+        var prevCity = document.createElement("button") 
+        //turns previously searched cities into buttons
+        //prevents duplicating locally stored items
+        console.log(cityInput.value)
+        if (storageArray.indexOf(cityInput.value) == -1) {
+          storageArray.push(cityInput.value);
+          localStorage.setItem('prevCity', JSON.stringify(cityInput.value));
+          // prevCity.textContent = cityInput.value;
+          // prevSearchEl.appendChild(prevCity);
+        }
+
+        prevCity.textContent = cityInput.value;
+        prevSearchEl.appendChild(prevCity);
+
+        for(var i = 0; i < prevSearchEl.children.length-1; i++) {
+          if (prevSearchEl.children[i].textContent === prevSearchEl.lastChild.textContent) {
+            prevSearchEl.removeChild(prevSearchEl.lastElementChild)
+          }
+        }
+
+        //adds style to each prevCity button
+        for (var i=0; i < prevSearchEl.children.length; i++) {
+          var child = prevSearchEl.children[i]
+          child.classList.add("btn")
+          child.classList.add("btn-secondary")
+        }
+
+        //adds event listener for locally stored previous searches
+        if (prevCity) {
+          prevCity.addEventListener('click', function() {
+            // console.log(prevCity.textContent)
+            cityInput.value = prevCity.textContent
+            // console.log(cityInput.value)
+            deleteChild()
+            getLocation();
+            resultsEl.setAttribute("style", "display: block")
+          })
+          
+        }
+        getforecastApi ()
+        getTodayApi()
+      })
+  
+
+  //shows search results elements
+  prevSearchEl.setAttribute("style", "display: block")
   cityInput.value = cityInput.value.replace("+", " ")
 }
 
 
+//prevents showing multiple city results at once
+function deleteChild() {
+  var deletetodayChild = document.querySelector(".todayResults");
+  deletetodayChild.innerHTML = "";
+  var deletefirstdayChild = document.querySelector(".firstDay");
+  deletefirstdayChild.innerHTML = "";
+  var deleteseconddayChild = document.querySelector(".secondDay");
+  deleteseconddayChild.innerHTML = "";
+  var deletethirddayChild = document.querySelector(".thirdDay");
+  deletethirddayChild.innerHTML = "";
+  var deletefourthdayChild = document.querySelector(".fourthDay");
+  deletefourthdayChild.innerHTML = "";
+  var deletefifthdayChild = document.querySelector(".fifthDay");
+  deletefifthdayChild.innerHTML = "";
+}
 
+
+//function to get info for 5 day forecast
 function getforecastApi() {
   var requestURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=e4f7fa74cca6af69d01f1e01654e6cc3'
 
@@ -179,8 +256,45 @@ function getforecastApi() {
 }
 
 
+//function to get today's weather info
+function getTodayApi() {
+  var requestURL = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=e4f7fa74cca6af69d01f1e01654e6cc3'
+
+  fetch(requestURL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonArray) {
+    console.log(jsonArray)
+
+    var date = document.createElement("h3");
+    var todayDate = moment.unix(jsonArray.dt).format("M/D/YYYY")
+    date.textContent = jsonArray.name + " (" + todayDate + ")"
+    todayResultsEl.appendChild(date);
+
+    var symbol = document.createElement("img");
+    symbol.src = 'http://openweathermap.org/img/wn/' + jsonArray.weather[0].icon + '@2x.png'
+    todayResultsEl.appendChild(symbol)
+
+    var temp = document.createElement("p")
+    temp.textContent = "Temp: " + jsonArray.main.temp + " °F"
+    todayResultsEl.appendChild(temp);
+
+    var wind = document.createElement("p")
+    wind.textContent = "Wind: " + jsonArray.wind.speed + " MPH"
+    todayResultsEl.appendChild(wind);
+
+    var humidity = document.createElement("p")
+    humidity.textContent = "Humidity: " + jsonArray.main.humidity + "%"
+    todayResultsEl.appendChild(humidity);
+
+  })
+}
+
+
+
 searchBtnEl.addEventListener("click", function() {
-  // getApi();
+  deleteChild()
   getLocation();
   resultsEl.setAttribute("style", "display: block")
 });
